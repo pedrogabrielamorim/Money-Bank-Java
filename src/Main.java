7d49b452aba378417ac4593a15dd8f0a43d459a5
Pedro Gabriel Amorim <pedroamorim7.pga@yahoo.com>
2023-08-10 17:37:35 -0300

    Atualização

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String nome = "Jhon Wick";
         String TipoConta = "Corrente";
-        double Saldo = 23000.58;
+        double saldo = 23000.58;
         int opcao = 0;
 
         System.out.println("Bem vindo(a) ao Money Bank");
@@ -11,45 +11,45 @@ public class Main {
         System.out.println("\nCliente: " + nome);
         System.out.println("Tipo de Conta: " + TipoConta);
 
-        System.out.println("Saldo atual:    R$ " + Saldo);
+        System.out.println("Saldo atual:    R$ " + saldo);
         System.out.println("\n**************************");
 
 
-            String menu = """
-                    ** Digite a opção desejada **\n
-                    1 - Consultar saldo
-                    2 - Transferir valor
-                    3 - Receber valor
-                    4 - sair
-                    
-                    """;
-            Scanner Leitura = new Scanner(System.in);
+        String menu = """
+                ** Digite a opção desejada **\n
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - sair
+                                    
+                """;
+        Scanner Leitura = new Scanner(System.in);
 
         while (opcao != 4) {
             System.out.println(menu);
             opcao = Leitura.nextInt();
-            if (opcao == 1 )  {
-                System.out.println("Saldo atual: " + Saldo);
+            if (opcao == 1) {
+                System.out.println("Saldo atual: " + saldo);
             } else if (opcao == 2) {
                 System.out.println("Qual o valor à transferido ?");
-                double Valor = Leitura.nextDouble();
-                if (Valor > Saldo) {
+                double valor = Leitura.nextDouble();
+                if (valor > saldo) {
                     System.out.println("Não há saldo suficiente para realizar a transferência.");
                 } else {
-                    Saldo -= Valor;
+                    saldo -= valor;
                     System.out.println("Transferência realizada!");
-                    System.out.println("Saldo atual: " + Saldo);
+                    System.out.println("Saldo atual: " + saldo);
                 }
-            }else if (opcao == 3){
-                    System.out.println("Valor recebido ?");
-                   double Valor = Leitura.nextDouble();
-                    Saldo += Valor;
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido ?");
+                double valor = Leitura.nextDouble();
+                saldo += valor;
                 System.out.println("Valor recebido com sucesso!");
-                    System.out.println("Saldo atual: " + Saldo);
-                }
-            else if (opcao != 4) {
+                System.out.println("Saldo atual: " + saldo);
+            } else if (opcao != 4) {
                 System.out.println("Opção inválida!");
+
+                }
             }
         }
     }
-}
